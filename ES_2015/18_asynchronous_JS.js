82243f1a81df5b861be0a510d4edb40369e500a6
Shubham Sharma <sh1nine96@gmail.com>
2022-10-19 04:59:55 +0530

    asynchronous js

---
+++ ES_2015/18_asynchronous_JS.js
@@ -0,0 +1,52 @@
+// Synchronous Programming
+
+// const name = "Miriam";
+// const greeting = `Hello, my name is ${name}!`;
+// console.log(greeting);
+// "Hello, my name is Miriam!"
+
+//  It would still be synchronous even if we called a separate function, like this:
+
+function makeGreeting(name) {
+  return `Hello, my name is ${name}!`;
+}
+
+const name = "Miriam";
+const greeting = makeGreeting(name);
+console.log(greeting);
+
+const MAX_PRIME = 1000000;
+
+function isPrime(n) {
+  for (let i = 2; i <= Math.sqrt(n); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return n > 1;
+}
+
+const random = (max) => Math.floor(Math.random() * max);
+
+function generatePrimes(quota) {
+  const primes = [];
+  while (primes.length < quota) {
+    const candidate = random(MAX_PRIME);
+    if (isPrime(candidate)) {
+      primes.push(candidate);
+    }
+  }
+  return primes;
+}
+
+const quota = document.querySelector("#quota");
+const output = document.querySelector("#output");
+
+document.querySelector("#generate").addEventListener("click", () => {
+  const primes = generatePrimes(quota.value);
+  output.textContent = `Finished generating ${quota.value} primes!`;
+});
+
+document.querySelector("#reload").addEventListener("click", () => {
+  document.location.reload();
+});
